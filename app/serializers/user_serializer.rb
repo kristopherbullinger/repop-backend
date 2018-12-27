bca98c79073629e106e353d6d9f247d95ba48e32
kristopherbullinger <kris.bullinger@gmx.com>
2018-12-27 18:30:19 -0500

    Update Purchases/Purchased information sent to client

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -10,11 +10,11 @@ class UserSerializer < ActiveModel::Serializer
   end
 
   def purchases
-    self.object.purchases.map{|z| z.review ? {id: z.id, item_id: z.item.id, rating: z.review.rating, description: z.review.description, review: true} : {id: z.id, item_id: z.item.id, review: false}}
+    self.object.purchases.map{|z| z.review ? {id: z.id, item_id: z.item.id, rating: z.review.rating, description: z.review.description, review: true, seller: {username: z.seller.username, id: z.seller.id}, purchase_date: z.created_at} : {id: z.id, item_id: z.item.id, review: false, seller: {username: z.seller.username, id: z.seller.id}, purchase_date: z.created_at}}
   end
 
   def purchased
-    self.object.purchased.map{|z| z.review ? {id: z.id, item_id: z.item.id, rating: z.review.rating, description: z.review.description, review: true} : {id: z.id, item_id: z.item.id, review: false}}
+    self.object.purchased.map{|z| z.review ? {id: z.id, item_id: z.item.id, rating: z.review.rating, description: z.review.description, review: true, purchaser: {username: z.purchaser.username, id: z.purchaser.id}, purchase_date: z.created_at} : {id: z.id, item_id: z.item.id, review: false, purchase_date: z.created_at}}
   end
 
 end
