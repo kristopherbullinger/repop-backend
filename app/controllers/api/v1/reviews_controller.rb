d45b1156724c9a802ef7843bf5307d141962f2be
kristopherbullinger <kris.bullinger@gmx.com>
2018-12-30 21:56:10 -0500

    Allow creation or edit of review.

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::ReviewsController < ApplicationController
+
+  def create
+    #if the review exists, attempt to update it.
+    if Review.find_by(purchase_id: review_params[:purchase_id])
+      @review = Review.find_by(purchase_id: review_params[:purchase_id])
+      #if update success, return user, else return errors
+      if @review.update(review_params)
+        render json: {currentUser: UserSerializer.new(@user)}, status: :ok
+      else
+        render json: {errors: ["There was an error updating this review."]}
+      end
+    #if review doesnt exist, attempt to create it
+    else
+      @review = Review.new(review_params)
+      if @review.save
+        render json: {currentUser: UserSerializer.new(@user)}, status: :ok
+      else
+        render json: {errors: ["There was an error updating this review."]}
+      end
+    end
+  end
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:purchase_id, :description, :rating)
+  end
 end
